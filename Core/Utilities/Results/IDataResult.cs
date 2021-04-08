859b5e9ef043d6eebab458962e5c70462fe3c01a
Baran Kurtbogan <barankurtbogan@gmail.com>
2021-04-08 05:14:27 +0300

    10.gun bitimi

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T>:IResult
+    {
+        //mesaj ve işlem sonucu (true veya false) IResult'dan alınacak, ek olarak T türünde bir data almalıyız
+        T Data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message):this(success) //this:  bu class demektir.. alttaki resultu da çalıştırır
+        {
+            Message = message; //getler ctor içinde set edilebilir... başka yerde edilemez
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
